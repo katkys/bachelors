32531540f9a82e26577e78469321bf83c9062d8f
katkys <k.kyselovaaa@gmail.com>
2026-02-26 13:10:54 +0100

    Update train_resnet_class_weights_ft.py

--- ModelTrainingExperiments/train_resnet_class_weights_ft.py
+++ ModelTrainingExperiments/train_resnet_class_weights_ft.py
@@ -1,3 +1,6 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
@@ -13,11 +16,11 @@ train_dir = "" #path to directory containing images from training dataset
 val_dir = "" #path to directory containing images from validation dataset
 test_dir = "" #path to directory containing images from test dataset
 
-BATCH_SIZE = 20
+BATCH_SIZE = 16
 IMG_SIZE = (224, 224)
-INITIAL_EPOCHS = 15
+INITIAL_EPOCHS = 10
 FINE_TUNE_EPOCHS = 10
-FT_LEARNING_RATE = 1e-5
+FT_LEARNING_RATE = 5e-6
 
 SAVE_MODEL = True
 EVALUATE_FINAL_MODEL = False
@@ -65,13 +68,13 @@ class_weights_array = compute_class_weight(
 )
 
 class_weights = dict(enumerate(class_weights_array))
-print("Class weights:", class_weights)
+# print("Class weights:", class_weights)
 
 data_augmentation = keras.Sequential([
     RandomFlip("horizontal"),
-    RandomRotation(0.08),
-    RandomZoom(0.08),
-    RandomTranslation(0.05, 0.05)],
+    RandomRotation(0.1),
+    RandomZoom(0.1),
+    RandomTranslation(0.08, 0.08)],
     name="data_augmentation"
 )
 
@@ -90,8 +93,7 @@ x = data_augmentation(inputs)
 x = preprocess_input(x)
 x = base_model(x, training=False)
 x = GlobalAveragePooling2D()(x)
-# x = BatchNormalization()(x)
-x = Dense(1024, activation='relu')(x)
+x = Dense(512, activation='relu')(x)
 x = Dropout(0.4)(x)
 outputs = Dense(classes_count, activation="softmax")(x) 
 model = keras.Model(inputs, outputs)
@@ -100,34 +102,22 @@ model.compile(optimizer=keras.optimizers.Adam(),
               loss=keras.losses.SparseCategoricalCrossentropy(),
                 metrics=['accuracy'])
 
+callbacks = [EarlyStopping(monitor="val_loss",patience=5, restore_best_weights=True)]
 if SAVE_MODEL:
-    history = model.fit(train_dataset,
-                        epochs=INITIAL_EPOCHS,
-                        validation_data=val_dataset,
-                        verbose=1,
-                        class_weight=class_weights,
-                        callbacks=[EarlyStopping(monitor="val_loss",patience=5, restore_best_weights=True),
-                                ModelCheckpoint(BEST_MODEL_SAVE_PATH, save_best_only=True, monitor="val_loss")])
-    with open(HISTORY_SAVE_PATH, 'wb') as file:
+    callbacks.append(ModelCheckpoint(BEST_MODEL_SAVE_PATH, save_best_only=True, monitor="val_loss"))
+
+history = model.fit(train_dataset,
+                    epochs=INITIAL_EPOCHS,
+                    validation_data=val_dataset,
+                    verbose=1,
+                    class_weight=class_weights,
+                    callbacks=callbacks)
+    
+with open(HISTORY_SAVE_PATH, 'wb') as file:
         pickle.dump(history.history, file)
-else:
-    history = model.fit(train_dataset,
-                        epochs=INITIAL_EPOCHS,
-                        validation_data=val_dataset,
-                        verbose=1,
-                        class_weight=class_weights,
-                        callbacks=[EarlyStopping(monitor="val_loss",patience=5, restore_best_weights=True)])
 
 # unfreeze the base model
 base_model.trainable = True
-
-# # freeze all layers except top x layers
-# fine_tune_at = len(base_model.layers) - 20 
-
-# for layer in base_model.layers[:fine_tune_at]:
-#     layer.trainable = False
-# for layer in base_model.layers[fine_tune_at:]:
-#     layer.trainable = True
 
 # also freeze BatchNormalization layers
 for layer in base_model.layers:
@@ -135,33 +125,26 @@ for layer in base_model.layers:
         layer.trainable = False
 
 #fine-tune 
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=FT_LEARNING_RATE), #very small learning rate
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=FT_LEARNING_RATE), 
               loss=keras.losses.SparseCategoricalCrossentropy(),
               metrics=['accuracy'])
 
+callbacks = [EarlyStopping(monitor="val_loss",patience=5, restore_best_weights=True)]
 if SAVE_MODEL:
-    history_ft = model.fit(train_dataset,
-                        epochs=INITIAL_EPOCHS+FINE_TUNE_EPOCHS,
-                        initial_epoch=history.epoch[-1]+1,
-                        validation_data=val_dataset,
-                        verbose=1,
-                        class_weight=class_weights,
-                        callbacks=[EarlyStopping(monitor="val_loss",patience=5,restore_best_weights=True), 
-                        ModelCheckpoint(BEST_MODEL_FT_SAVE_PATH, save_best_only=True, monitor="val_loss")])
-
-    with open(HISTORY_FT_SAVE_PATH, 'wb') as file:
-        pickle.dump(history_ft.history, file)
-else:
-    history_ft = model.fit(train_dataset,
+    callbacks.append(ModelCheckpoint(BEST_MODEL_FT_SAVE_PATH, save_best_only=True, monitor="val_loss"))
+    
+history_ft = model.fit(train_dataset,
             epochs=INITIAL_EPOCHS+FINE_TUNE_EPOCHS,
             initial_epoch=history.epoch[-1]+1,
             validation_data=val_dataset,
             verbose=1,
             class_weight=class_weights,
-            callbacks=[EarlyStopping(monitor="val_loss",patience=5,restore_best_weights=True)])
+            callbacks=callbacks)
 
-# Plot training curves
-# with fine-tuning, we have to combine the histories into one for visualization:
+with open(HISTORY_FT_SAVE_PATH, 'wb') as file:
+        pickle.dump(history_ft.history, file)
+
+# Training curves
 combined_history = {}
 for k in history.history.keys():
     combined_history[k] = history.history[k] + history_ft.history[k]
@@ -170,11 +153,20 @@ eval.plot_training_history(
     save_path="training_loss_acc.png"
 )
 
+metrics = eval.get_best_epoch_metrics(combined_history)
+
+print("\n===== MODEL METRICS (model selected based on min val loss) =====")
+print(f"Epoch: {metrics['epoch']}")
+print(f"Validation loss: {metrics['val_loss']:.4f}")
+print(f"Validation accuracy: {metrics['val_accuracy']:.4f}")
+print(f"Training loss: {metrics['train_loss']:.4f}")
+print(f"Training accuracy: {metrics['train_accuracy']:.4f}")
+print("=================================================================")
+
 if EVALUATE_FINAL_MODEL:
     test_loss, test_acc = model.evaluate(test_dataset)
     print(f"\nTEST RESULTS:\nTest accuracy: {test_acc:.4f}\nTest loss: {test_loss:.4f}")
 
-    # Get class names (label -> class mapping)
     class_names = train_dataset.class_names
 
     # Confusion matrix
@@ -203,4 +195,3 @@ if EVALUATE_FINAL_MODEL:
         class_names=class_names,
         save_path="roc_curves.png"
     )
-
