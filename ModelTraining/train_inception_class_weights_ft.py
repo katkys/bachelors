39c5266cf4e8d2f8f12b97f81cf8b73c146d57c5
katkys <k.kyselovaaa@gmail.com>
2026-03-16 17:48:00 +0100

    Rename folder ModelTrainingExperiments to ModelTraining

---
+++ ModelTraining/train_inception_class_weights_ft.py
@@ -0,0 +1,169 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, RandomFlip, RandomRotation, RandomZoom, RandomTranslation
+from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.utils.class_weight import compute_class_weight
+import numpy as np
+import pickle
+import evaluation as eval
+
+train_dir = "" #path to directory containing images from training dataset
+val_dir = "" #path to directory containing images from validation dataset
+
+IMG_SIZE = (299, 299)
+
+BATCH_SIZE = 16
+INITIAL_EPOCHS = 10
+FINE_TUNE_EPOCHS = 10
+FT_LEARNING_RATE = 1e-5
+EARLY_STOP_PATIENCE = 4
+
+SAVE_MODEL = False
+MODEL_ID = "M0X_class_weights" #an identificator for the model used for naming the output directory where history files and train/val plot will be saved
+SAVE_DIR_PATH = f"./models_and_histories/{MODEL_ID}"
+os.makedirs(SAVE_DIR_PATH, exist_ok=True)
+
+RANDOM_SEED = 27
+
+BEST_MODEL_SAVE_PATH = f"{SAVE_DIR_PATH}/inception_best_model_before_ft.keras"
+BEST_MODEL_FT_SAVE_PATH = f"{SAVE_DIR_PATH}/inception_best_model_after_ft.keras"
+HISTORY_SAVE_PATH = f"{SAVE_DIR_PATH}/inception_train_history_before_ft.pkl"
+HISTORY_FT_SAVE_PATH = f"{SAVE_DIR_PATH}/inception_train_history_after_ft.pkl"
+
+train_dataset = image_dataset_from_directory(
+    train_dir,
+    labels='inferred',
+    label_mode='int',
+    batch_size=BATCH_SIZE,
+    image_size=IMG_SIZE,
+    seed=RANDOM_SEED
+)
+
+val_dataset = image_dataset_from_directory(
+    val_dir,
+    labels='inferred',
+    label_mode='int',
+    batch_size=BATCH_SIZE,
+    image_size=IMG_SIZE,
+    seed=RANDOM_SEED
+) 
+
+classes_count = len(train_dataset.class_names)
+
+y_train = np.concatenate([y.numpy() for x,y in train_dataset], axis=0)
+class_weights_array = compute_class_weight(
+    class_weight='balanced',
+    classes=np.arange(classes_count),
+    y=y_train
+)
+
+class_weights = dict(enumerate(class_weights_array))
+
+data_augmentation = keras.Sequential([
+    RandomFlip("horizontal"),
+    RandomRotation(0.08),
+    RandomZoom(0.08),
+    RandomTranslation(0.05, 0.05)],
+    name="data_augmentation"
+)
+
+#load the InceptionV3 model pre-trained on ImageNet
+base_model = InceptionV3(
+    weights='imagenet',
+    input_shape=(IMG_SIZE[0], IMG_SIZE[1], 3),
+    include_top=False)
+
+#freeze the base model
+base_model.trainable = False
+
+#create a new model on top
+inputs = keras.Input(shape=(IMG_SIZE[0], IMG_SIZE[1], 3))
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = base_model(x, training=False)
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.4)(x)
+outputs = Dense(classes_count, activation="softmax")(x) 
+model = keras.Model(inputs, outputs)
+
+model.compile(optimizer=keras.optimizers.Adam(),
+              loss=keras.losses.SparseCategoricalCrossentropy(),
+                metrics=['accuracy'])
+
+callbacks=[EarlyStopping(monitor="val_loss",patience=EARLY_STOP_PATIENCE, restore_best_weights=True)]
+if SAVE_MODEL:
+    callbacks.append(ModelCheckpoint(BEST_MODEL_SAVE_PATH, save_best_only=True, monitor="val_loss"))
+
+
+history = model.fit(train_dataset,
+                    epochs=INITIAL_EPOCHS,
+                    validation_data=val_dataset,
+                    verbose=1,
+                    class_weight=class_weights,
+                    callbacks=callbacks)
+
+with open(HISTORY_SAVE_PATH, 'wb') as file:
+    pickle.dump(history.history, file)
+
+#unfreeze the base model
+base_model.trainable = True
+
+#freeze batch-normalization layers
+for layer in base_model.layers:
+    if isinstance(layer, keras.layers.BatchNormalization):
+        layer.trainable = False
+
+#fine-tune 
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=FT_LEARNING_RATE), 
+              loss=keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+callbacks=[EarlyStopping(monitor="val_loss",patience=EARLY_STOP_PATIENCE, restore_best_weights=True)]
+if SAVE_MODEL:
+    callbacks.append(ModelCheckpoint(BEST_MODEL_FT_SAVE_PATH, save_best_only=True, monitor="val_loss"))
+
+
+history_ft = model.fit(train_dataset,
+                    epochs=INITIAL_EPOCHS+FINE_TUNE_EPOCHS,
+                    initial_epoch=history.epoch[-1]+1,
+                    validation_data=val_dataset,
+                    verbose=1,
+                    class_weight=class_weights,
+                    callbacks=callbacks)
+
+with open(HISTORY_FT_SAVE_PATH, 'wb') as file:
+    pickle.dump(history_ft.history, file)
+
+#Training curves
+combined_history = {}
+for k in history.history.keys():
+    combined_history[k] = history.history[k] + history_ft.history[k]
+eval.plot_training_history(
+    combined_history,
+    save_path="training_loss_acc.png"
+)
+
+
+val_losses = np.array(combined_history["val_loss"])
+best_epoch_idx = np.argmin(val_losses)
+    
+metrics = {
+        "epoch": int(best_epoch_idx + 1),
+        "val_loss": float(combined_history["val_loss"][best_epoch_idx]),
+        "val_accuracy": float(combined_history["val_accuracy"][best_epoch_idx]),
+        "train_loss": float(combined_history["loss"][best_epoch_idx]),
+        "train_accuracy": float(combined_history["accuracy"][best_epoch_idx])}
+
+print("\n MODEL METRICS (model selected based on min val loss):")
+print(f"Epoch: {metrics['epoch']}")
+print(f"Validation loss: {metrics['val_loss']:.4f}")
+print(f"Validation accuracy: {metrics['val_accuracy']:.4f}")
+print(f"Training loss: {metrics['train_loss']:.4f}")
+print(f"Training accuracy: {metrics['train_accuracy']:.4f}")
+
---
+++ ModelTraining/train_inception_without_ft.py
@@ -0,0 +1,129 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, RandomFlip, RandomRotation, RandomZoom, RandomTranslation
+from tensorflow.keras.utils import image_dataset_from_directory, set_random_seed
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import pickle
+import evaluation as eval
+
+train_dir = "" #path to directory containing images from training dataset
+val_dir = "" #path to directory containing images from validation dataset
+
+IMG_SIZE = (299, 299)
+
+BATCH_SIZE = 16
+INITIAL_EPOCHS = 15
+LABEL_SMOOTHING = 0.00
+EARLY_STOP_PATIENCE = 4
+
+SAVE_MODEL = False
+MODEL_ID = "M0X" #an identificator for the model used for naming the output directory where history files and train/val plot will be saved
+SAVE_DIR_PATH = f"./models_and_histories/{MODEL_ID}"
+os.makedirs(SAVE_DIR_PATH, exist_ok=True)
+
+RANDOM_SEED = 27
+
+BEST_MODEL_SAVE_PATH = f"{SAVE_DIR_PATH}/inception_base_model.keras"
+HISTORY_SAVE_PATH = f"{SAVE_DIR_PATH}/inception_base_train_history.pkl"
+
+set_random_seed(RANDOM_SEED)
+
+train_dataset = image_dataset_from_directory(
+    train_dir,
+    labels='inferred',
+    label_mode='categorical',
+    batch_size=BATCH_SIZE,
+    image_size=IMG_SIZE,
+    shuffle=True,
+    seed=RANDOM_SEED
+)
+
+val_dataset = image_dataset_from_directory(
+    val_dir,
+    labels='inferred',
+    label_mode='categorical',
+    batch_size=BATCH_SIZE,
+    image_size=IMG_SIZE,
+    shuffle=True,
+    seed=RANDOM_SEED
+)  
+
+classes_count = len(train_dataset.class_names)
+
+data_augmentation = keras.Sequential([
+    RandomFlip("horizontal"),
+    RandomRotation(0.08),
+    RandomZoom(0.08),
+    RandomTranslation(0.05, 0.05)],
+    name="data_augmentation"
+)
+
+#load the InceptionV3 model pre-trained on ImageNet
+base_model = InceptionV3(
+    weights='imagenet',
+    input_shape=(IMG_SIZE[0], IMG_SIZE[1], 3),
+    include_top=False)
+
+#freeze the base model
+base_model.trainable = False
+
+#create a new model on top
+inputs = keras.Input(shape=(IMG_SIZE[0], IMG_SIZE[1], 3))
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = base_model(x, training=False)
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.4)(x)
+outputs = Dense(classes_count, activation="softmax")(x) 
+model = keras.Model(inputs, outputs)
+
+metrics = [
+    "accuracy",
+    keras.metrics.Precision(name="precision"),
+    keras.metrics.Recall(name="recall"),
+    keras.metrics.F1Score(average="macro", name="f1_score")
+]
+
+model.compile(optimizer=keras.optimizers.Adam(),
+              loss=keras.losses.CategoricalCrossentropy(label_smoothing=LABEL_SMOOTHING),
+                metrics=metrics)
+
+callbacks = [EarlyStopping(monitor="val_loss",patience=EARLY_STOP_PATIENCE, restore_best_weights=True)]
+if SAVE_MODEL:
+    callbacks.append(ModelCheckpoint(BEST_MODEL_SAVE_PATH, monitor="val_f1_score", mode="max", save_best_only=True))
+
+history = model.fit(train_dataset,
+                    epochs=INITIAL_EPOCHS,
+                    validation_data=val_dataset,
+                    verbose=1,
+                    callbacks=callbacks)
+    
+with open(HISTORY_SAVE_PATH, 'wb') as file:
+        pickle.dump(history.history, file)
+    
+# Training curves
+eval.plot_training_history(
+    history.history,
+    save_path="training_loss_acc.png"
+)
+
+best_epoch_criterium = "val_f1_score" # or val_loss / val_accuracy
+metrics = eval.get_best_epoch_metrics(history.history, criterium=best_epoch_criterium)
+
+print(f"\nMODEL METRICS (model selected based on {best_epoch_criterium}):")
+print(f"Epoch: {metrics['epoch']}")
+print(f"Validation loss: {metrics['val_loss']:.4f}")
+print(f"Validation accuracy: {metrics['val_accuracy']:.4f}")
+print(f"Validation precision: {metrics['val_precision']:.4f}")
+print(f"Validation recall: {metrics['val_recall']:.4f}")
+print(f"Validation F1-score: {metrics['val_f1']:.4f}")
+print(f"Training loss: {metrics['train_loss']:.4f}")
+print(f"Training accuracy: {metrics['train_accuracy']:.4f}")
+print(f"Training precision: {metrics['train_precision']:.4f}")
+print(f"Training recall: {metrics['train_recall']:.4f}")
+print(f"Training F1-score: {metrics['train_f1']:.4f}")
